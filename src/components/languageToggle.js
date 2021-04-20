2a0096fb3928cfed5541559aea25afb2cc53f73e
Qiao Lin <qiaoclin@gmail.com>
2021-04-20 15:43:26 +0200

    add press release

--- src/components/languageToggle.js
+++ src/components/languageToggle.js
@@ -1,13 +1,15 @@
-import { Link, useI18next } from "gatsby-plugin-react-i18next";
+import { Link, useI18next, Trans } from "gatsby-plugin-react-i18next";
 import React from "react";
 import clsx from "clsx";
+import DEPressRelease from "../assets/pdfs/Terrestrial Assemblage_Pressemitteilung.pdf";
+import ENPRessRelease from "../assets/pdfs/Terrestrial Assemblage_pressrelease.pdf";
 
 const LanguageToggle = () => {
   const { languages, originalPath, language } = useI18next();
-
+  const isGerman = language === "de";
   return (
     <header className="main-header">
-      <ul className="flex flex-row my-5">
+      <ul className="flex flex-row mt-5">
         {languages.map((lng) => (
           <li
             key={lng}
@@ -22,6 +24,13 @@ const LanguageToggle = () => {
           </li>
         ))}
       </ul>
+      <a
+        href={isGerman ? DEPressRelease : ENPRessRelease}
+        download
+        className="text-lg text-white uppercase hover:text-algea"
+      >
+        <Trans>Press</Trans>
+      </a>
     </header>
   );
 };
