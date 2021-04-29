a1e3f22fd2c95e22244d9d0d97873ba983d9188e
Qiao Lin <qiaoclin@gmail.com>
2021-04-29 23:29:15 +0200

    add curators

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,8 +2,10 @@ import * as React from "react";
 import { useI18next } from "gatsby-plugin-react-i18next";
 import AboutSVG from "../assets/svgs/headers/en/subheader/About.svg";
 import ExhibitionSVG from "../assets/svgs/headers/en/subheader/Exhibition-Venue.svg";
+import CuratorSVG from "../assets/svgs/headers/en/subheader/Curators.svg";
 import DEAboutSVG from "../assets/svgs/headers/de/subheader/Uber.svg";
 import DEExhibitionSVG from "../assets/svgs/headers/de/subheader/Austellungsort.svg";
+import DECuratorSVG from "../assets/svgs/headers/de/subheader/Kuratorinnen.svg";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import BackgroundImage from "../components/background/bgAbout";
@@ -25,30 +27,36 @@ const AboutPage = ({ data }) => {
     n.fileAbsolutePath.includes(`/${language}/main/exhibitionVenue`)
   );
 
+  const curatorsMDNode = data.allMarkdownRemark.nodes.find((n) =>
+    n.fileAbsolutePath.includes(`/${language}/main/curators`)
+  );
+
+  const AboutVector = isGerman ? DEAboutSVG : AboutSVG;
+  const ExhibitionVector = isGerman ? DEExhibitionSVG : ExhibitionSVG;
+  const CuratorVector = isGerman ? DECuratorSVG : CuratorSVG;
+
   return (
     <Layout>
       <BackgroundImage />
       <section className="md:grid md:grid-cols-10 gap-4 m-10">
         <div className="md:col-span-5 ">
-          {isGerman ? (
-            <DEAboutSVG className="max-w-full max-h-36 mb-5" />
-          ) : (
-            <AboutSVG className="max-w-full max-h-36 mb-5" />
-          )}
+          <AboutVector className={CLASSES.svgHeader} />
           <div className={CLASSES.textBox}>
             <div dangerouslySetInnerHTML={{ __html: aboutMDNode.html }} />
           </div>
         </div>
-        <div className="md:col-span-5 md:col-start-1 md:py-10">
-          {isGerman ? (
-            <DEExhibitionSVG className="max-h-36 mb-5 max-w-full" />
-          ) : (
-            <ExhibitionSVG className="max-h-36 mb-5 max-w-full" />
-          )}
+        <div className="md:col-span-5 md:col-start-1 md:py-5">
+          <ExhibitionVector className={CLASSES.svgHeader} />
           <div className={CLASSES.textBox}>
             <div
               dangerouslySetInnerHTML={{ __html: exhibitionVenueMDNode.html }}
             />
+          </div>
+        </div>
+        <div className="md:col-span-5 md:col-start-1 md:py-5">
+          <CuratorVector className={CLASSES.svgHeader} />
+          <div className={CLASSES.textBox}>
+            <div dangerouslySetInnerHTML={{ __html: curatorsMDNode.html }} />
           </div>
         </div>
       </section>
