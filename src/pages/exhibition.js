ea67d0610c56fd6e23eaa4904862e09e22849013
Qiao Lin <qiaoclin@gmail.com>
2021-05-03 18:24:55 +0200

    carousel with proper settings

--- src/pages/exhibition.js
+++ src/pages/exhibition.js
@@ -1,36 +1,79 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import * as styles from "../styles/exhibition.css"; //eslint-disable-line no-unused-vars
+import NameVector from "../components/nameVector";
 
 const ExhibitionPage = ({ data }) => {
   const clImages = data.images.edges;
+  const [activeArrow, setActiveArrow] = useState();
+  const sliderRef = useRef();
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const settings = {
-    dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
+    variableWidth: true,
     slidesToScroll: 1,
     centerMode: true,
-    className: "justify-center",
+    lazyLoad: true,
+    afterChange: (current) => setActiveIndex(current),
+  };
+
+  const artistKey = clImages[activeIndex].node.public_id.split("/")[2];
+
+  const checkMousePosition = (event) => {
+    const posX = event.pageX;
+    const pageWidth = window.innerWidth;
+    if (posX < pageWidth / 4) {
+      setActiveArrow("left");
+    } else if (posX > pageWidth * 0.75) {
+      setActiveArrow("right");
+    } else {
+      setActiveArrow(undefined);
+    }
+  };
+
+  const swipe = () => {
+    if (activeArrow === "left") {
+      sliderRef.current.slickPrev();
+    } else if (activeArrow === "right") {
+      sliderRef.current.slickNext();
+    }
   };
 
   return (
     <Layout>
-      <main>
-        <Slider {...settings}>
-          {clImages.map((image) => (
-            <img
-              className="w-auto h-54"
-              key={image.node.public_id}
-              src={image.node.secure_url}
-              alt={image.key}
-            />
-          ))}
+      <main
+        onMouseMove={checkMousePosition}
+        onClick={swipe}
+        className="bg-black"
+      >
+        <section className="text-center align-center">
+          <NameVector
+            identifier={artistKey}
+            className="block h-14 md:h-24 md:inline mx-auto md:mx-0"
+          />
+        </section>
+        <Slider {...settings} className={activeArrow} ref={sliderRef}>
+          {clImages.map((image) => {
+            const ratio = image.node.width / image.node.height;
+            const width = ratio * 500;
+            return (
+              <div style={{ width }} key={image.node.public_id}>
+                <img
+                  className="mx-1 px-1 focus:outline-none"
+                  src={image.node.secure_url}
+                  alt={image.key}
+                />
+              </div>
+            );
+          })}
         </Slider>
       </main>
     </Layout>
@@ -59,6 +102,8 @@ export const query = graphql`
         node {
           secure_url
           public_id
+          width
+          height
         }
       }
     }
