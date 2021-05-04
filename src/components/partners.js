6807fe919d5a70b157b61b1d9c0798aab333e29a
Qiao Lin <qiaoclin@gmail.com>
2021-05-04 13:46:50 +0200

    cropped partner logo

--- src/components/partners.js
+++ src/components/partners.js
@@ -6,10 +6,11 @@ function Partners() {
   const { t } = useTranslation();
 
   return (
-    <section className="m-10">
+    <section className="m-10 wi">
       <div className="flex mt-5 justify-end">
         <StaticImage
-          height={50}
+          height={75}
+          objectFit="contain"
           className="mr-3"
           alt={t("Berlin Senate Department for Culture and Europe")}
           src={"../assets/images/partners/Sen_KuEu.png"}
@@ -17,30 +18,28 @@ function Partners() {
         <StaticImage
           height={50}
           className="mr-3"
+          objectFit="contain"
           alt={t("Korean Cultural Centre Berlin")}
           src={"../assets/images/partners/KoreanCenter.png"}
         />
         <StaticImage
-          height={50}
+          height={60}
           className="mr-3"
-          alt={t("Green Belt Europe")}
-          src={"../assets/images/partners/GBEurope.png"}
-        />
-        <StaticImage
-          height={50}
-          className="mr-3"
+          objectFit="contain"
           alt={t("Hanns Seidel Stiftung")}
           src={"../assets/images/partners/HSS_1.png"}
         />
         <StaticImage
-          height={50}
+          height={65}
           className="mr-3"
+          objectFit="contain"
           alt={t("BUND")}
           src={"../assets/images/partners/BUND.png"}
         />
         <StaticImage
           height={50}
           className="mr-3"
+          objectFit="contain"
           alt={t("Floating University")}
           src={"../assets/images/partners/floatingev.png"}
         />
